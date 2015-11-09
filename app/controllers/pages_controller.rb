358c1e5a20671fd9ef4d4e8baf911f79dcb71ea6
Brent Fisk <brentallenfisk@gmail.com>
2015-11-09 21:53:15 +0000

    Ready for first deploy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,17 @@ class PagesController < ApplicationController
     
     def home
     end
+    
+    def about
+    end
+    
+    def work
+    end
+    
+    def skills
+    end
+    
+    def blog
+    end
+
 end
